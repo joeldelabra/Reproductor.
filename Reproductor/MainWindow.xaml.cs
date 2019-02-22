dfe7529f67ee6ba2371ee8cc0d67c04448e3790e
Luis Agustin Parra Valenzuela <luis.agustin.00@hotmail.com>
2019-02-22 12:34:06 -0700

    Agregar funcones: Pausa, Play & Detner.

--- Reproductor/MainWindow.xaml.cs
+++ Reproductor/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using Microsoft.Win32;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
 
 namespace Reproductor
 {
@@ -22,9 +24,30 @@ namespace Reproductor
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        AudioFileReader reader;
+        // NUuestra comunicacion con la tarjeta de sonido.
+        WaveOutEvent output;
+         
+    
+
         public MainWindow()
         {
             InitializeComponent();
+            LlenarComboSalida();
+        }
+
+        private void LlenarComboSalida()
+        {
+            cbSalida.Items.Clear();
+            for (int i=0; i < WaveOut.DeviceCount; i++)
+            {
+                WaveOutCapabilities Capacidades =
+                     WaveOut.GetCapabilities(i);
+                cbSalida.Items.Add(Capacidades.ProductName);
+            }
+
+            cbSalida.SelectedIndex = 0;
         }
 
         private void btnElegirArchivo_Click(object sender, RoutedEventArgs e)
@@ -36,6 +59,78 @@ namespace Reproductor
                 txtRutaArchivo.Text =
                      openFileDialog.FileName;
             }
+        }
+
+        private void btnReproducir_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (output != null && output.PlaybackState == PlaybackState.Paused)
+            {
+                output.Play();
+
+                btnDetener.IsEnabled = true;
+                btnPausa.IsEnabled = true;
+                btnReproducir.IsEnabled = false;
+
+            }
+            else
+            {
+                reader =
+                    new AudioFileReader(txtRutaArchivo.Text);
+                output =
+                    new WaveOutEvent();
+
+                output.DeviceNumber = cbSalida.SelectedIndex;
+
+                output.PlaybackStopped += Output_PlaybackStopped;
+
+                output.Init(reader);
+                output.Play();
+
+                btnDetener.IsEnabled = true;
+                btnPausa.IsEnabled = true;
+                btnReproducir.IsEnabled = false;
+
+                lblTiempoFinal.Text =
+                    reader.TotalTime.ToString().Substring(0,8);
+
+            }
+        }
+
+        private void Output_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            reader.Dispose();
+            output.Dispose();
+        }
+
+        private void btnPausa_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != null)
+            {
+                output.Pause();
+
+                btnDetener.IsEnabled = true;
+                btnPausa.IsEnabled = false;
+                btnReproducir.IsEnabled = true;
+            }
+
+        }
+
+        private void btnDetener_Click(object sender, RoutedEventArgs e)
+        {
+            if (output != null)
+            {
+                output.Stop();
+
+                btnDetener.IsEnabled = false;
+                btnPausa.IsEnabled = false;
+                btnReproducir.IsEnabled = true;
+            }
+        }
+
+        private void cbSalida_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
